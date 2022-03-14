6de399f5ba97a68e0a255e8963bd7b25e2e904b8
Miquel Cruz <cruz.miquel@gmail.com>
2022-03-14 13:06:47 +0100

    answerCard

---
+++ src/Components/Molecules/AnswerCard/AnswerCard.jsx
@@ -0,0 +1,11 @@
+
+
+export function AnswerCard({answer}) {
+    return (
+        <div className="card-container">
+  
+             <div>{answer.textAnswer}</div>
+        </div>
+      );
+    
+}
--- src/Components/Molecules/QuestionCard/QuestionCard.jsx
+++ src/Components/Molecules/QuestionCard/QuestionCard.jsx
@@ -1,9 +1,33 @@
-export function QuestionCard({ question }){
-    return (
-      <div className="card-container">
+import { useState, useEffect } from "react";
+import { ApiService } from "../../../Services/APIService";
+import { useParams } from "react-router-dom"; 
+import { AnswerCard } from "../AnswerCard/AnswerCard";
 
-            <div>{question.challengeQuestion}</div>
-        
-      </div>
-    );
+export function QuestionCard({ question }) {
+  const [answers, setAnswers] = useState([]);
+
+  let questionId = useParams();
+
+  useEffect(() => {
+    const paramsId = questionId.id;
+    console.log(paramsId);
+    ApiService()
+      .getAnswersByQuestionID(paramsId)
+      .then((res) => setAnswers(res.data))
+      .catch((error) => console.log(error.response));
+  }, [questionId.id]);
+
+  return (
+    <div className="card-container">
+      <div>{question.challengeQuestion}</div>
+
+      <ul>
+        {answers.map((answer, index) => (
+          <li key={index}>
+            <AnswerCard answer={answer} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
