bc717c25ce2631e80e3854eb3703ac4d75418737
SergiFuentes <you@example.com>
2022-03-22 17:32:02 +0100

    useState dataIsOk in UserTotalScore

--- src/Components/Organisms/UserTotalScore/UserTotalScore.jsx
+++ src/Components/Organisms/UserTotalScore/UserTotalScore.jsx
@@ -2,42 +2,38 @@ import { useState, useEffect } from "react";
 import { ApiService } from "../../../Services/APIService";
 import { useParams } from "react-router-dom";
 
- 
- 
- export function UserTotalScore() {
-    const [scores, setScores] = useState([]);
-  
-   let challenge = useParams();
-   const challengeId = challenge.id;
-   const [dataIsOk, setdataIsOk] = useState(false);
+export function UserTotalScore() {
+  const [scores, setScores] = useState([]);
 
-    useEffect(() => {
-      ApiService()
-        .getUserTotalScore( challengeId ).then( ( res ) => {
-          setScores( res.data)
-        setdataIsOk(true)}
-         )
-        .catch( error => console.log( error.response ) );
-         
-    },[]);
+  let challenge = useParams();
+  const challengeId = challenge.id;
+  const [dataIsOk, setdataIsOk] = useState(false);
 
-    console.log(scores)
-    return (
-      
-      <div className="">
-  {dataIsOk ? 
-  <div>
-         <p> Name: {scores.user.userName}</p>
-        <p> Challenge: {scores.challenge.challengeName}</p>
-        <p> Correct answers: {scores.correctAnswers}</p>
-        <p> Incorrect answers: {scores.incorrectAnswers}</p>
-      </div>
-      :
-      <p>"loading"</p>
-      } 
-      </div> 
-    );
-  }
+  useEffect(() => {
+    ApiService()
+      .getUserTotalScore(challengeId)
+      .then((res) => {
+        setScores(res.data);
+        setdataIsOk(true);
+      })
+      .catch((error) => console.log(error.response));
+  }, []);
 
-  export default UserTotalScore;
-  
+  console.log(scores);
+  return (
+    <div className="">
+      {dataIsOk ? (
+        <div>
+          <p> Name: {scores.user.userName}</p>
+          <p> Challenge: {scores.challenge.challengeName}</p>
+          <p> Correct answers: {scores.correctAnswers}</p>
+          <p> Incorrect answers: {scores.incorrectAnswers}</p>
+        </div>
+      ) : (
+        <p>"loading"</p>
+      )}
+    </div>
+  );
+}
+
+export default UserTotalScore;
