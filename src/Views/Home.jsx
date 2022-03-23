6dfb38ae8f5c70ac0edb66d5c1c1d697f4ff71fd
Miquel Cruz <cruz.miquel@gmail.com>
2022-03-23 07:17:51 +0100

    adjust views

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,6 +1,6 @@
 import "./Home.css";
 import ChallengeLogo2 from "../Assets/ChallengeLogo2.png";
-
+import BlogPostBro from  "../Assets/BlogPostBro.png";
 
 function Home() {
   return (
@@ -8,6 +8,7 @@ function Home() {
       <div className="homeContainer">
       <a href="/challenges">
        <img  className="logoHome" src={ChallengeLogo2} alt="" width="100%"/>
+       <img  className="blogPostBro" src={BlogPostBro} alt="" width="100%"/>
        </a>
        
       </div>
