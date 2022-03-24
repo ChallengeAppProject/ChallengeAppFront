b0790390c8f166dded90b460f37748a679c6bb4c
Rubén <rubencaporte@gmail.com>
2022-03-24 14:34:38 +0100

    adminlanding

--- src/Views/Pages/Admin/AdminLanding.jsx
+++ src/Views/Pages/Admin/AdminLanding.jsx
@@ -2,18 +2,17 @@ import Navbar from "../../../Components/Molecules/Header/Navbar";
 import Footer from "../../../Components/Molecules/Footer/Footer";
 import { Link } from "react-router-dom";
 import "../Admin/AdminLanding.css";
-import {ChallengeList} from "./../../../Components/Organisms/ChallengeList/ChallengeList";
+import { ChallengeList } from "./../../../Components/Organisms/ChallengeList/ChallengeList";
 function AdminLanding() {
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-      <div className="form-group my-3">
-       
-        <ChallengeList/>
-        <Link to="/create/challenge" className="btnchll">
-          Create Challenge
-        </Link>
+        <div className="form-group my-3">
+          <Link to="/create/challenge" className="btnchll">
+            Create Challenge
+          </Link>
+          <ChallengeList />
         </div>
       </div>
       <Footer />
