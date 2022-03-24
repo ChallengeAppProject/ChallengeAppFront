e7faa7802ad786a089aea8c661d0eb6585d3cd19
SergiFuentes <you@example.com>
2022-03-24 09:39:49 +0100

    wellcome title setted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h1>Welcome to ChallengeApp</h1>
+      <h1>Wellcome to ChallengeApp</h1>
       <ChallengeList/>
       <Footer/>
     </div>
--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders Welcome to ChallengeApp', () => {
+test('renders Wellcome to ChallengeApp', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to ChallengeApp/i);
+  const linkElement = screen.getByText(/Wellcome to ChallengeApp/i);
   expect(linkElement).toBeInTheDocument();
 });
 
