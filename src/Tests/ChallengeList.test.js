846b8f633192a7968860fc8a8fe56888111f58df
Rubén <rubencaporte@gmail.com>
2022-03-28 20:25:19 +0200

    test challegeList fixed

--- src/Tests/ChallengeList.test.js
+++ src/Tests/ChallengeList.test.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { ChallengeList } from '../Components/Organisms/ChallengeList/ChallengeList';
-import { expect } from 'chai';
-import sinon from 'sinon';
-import { shallow } from 'enzyme';
-import {ChallengeCard} from '../Components/Molecules/ChallengeCard/ChallengeCard' 
-import { QuestionCard } from '../Components/Molecules/QuestionCard/QuestionCard';
-import { mockComponent } from 'react-dom/test-utils';
+import React from "react";
+import { ChallengeList } from "../Components/Organisms/ChallengeList/ChallengeList";
+import { expect } from "chai";
+import sinon from "sinon";
+import { shallow } from "enzyme";
+import { ChallengeCard } from "../Components/Molecules/ChallengeCard/ChallengeCard";
+import { QuestionCard } from "../Components/Molecules/QuestionCard/QuestionCard";
+import { mockComponent } from "react-dom/test-utils";
 
-describe('<ChallengeList />', () => {
-  it('renders a wellcome title', () => {
+describe("<ChallengeList />", () => {
+  it("renders a wellcome title", () => {
     const wrapper = shallow(<ChallengeList />);
-    expect(wrapper.contains(<h1 className="wellcomeTitle">Challenges</h1>)).to.equal(true);
-  } );
-  
-});  
+    expect(
+      wrapper.contains(
+        <h2 className="challengesTitle">These are our available Challenges:</h2>
+      )
+    ).to.equal(true);
+  });
+});
